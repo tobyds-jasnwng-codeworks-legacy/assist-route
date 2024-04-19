e0d1592806c0c91ef8dbd90bf7119daa7f99d06a
yulia <langedoc@gmail.com>
2024-04-19 12:10:09 +0200

    feat: dropdown list working, route info display working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
 import DropdownListRoutes from './components/DropdownListRoutes'
 import RouteInfoDisplay from './components/RouteInfoDisplay'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [routes, setRoutes] = useState([
+    {
+      id: 1,
+      name: "R5 SABADELL",
+      stops: [
+        {
+          id: 1,
+          name: "BARBERÀ DEL VALLÈS Carretera de Barcelona, 280"
+        },
+        {
+          id: 2,
+          name: "SABADELL Carretera de Barcelona, 621"
+        },
+        {
+          id: 3,
+          name: "SABADELL Av. Barberà, 414"
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: "R5 SABADELL RETURN",
+      stops: [
+        {
+          id: 1,
+          name: "SABADELL Av. Barberà, 414"
+        },
+        {
+          id: 2,
+          name: "SABADELL Carretera de Barcelona, 621"
+        },
+        {
+          id: 3,
+          name: "BARBERÀ DEL VALLÈS Carretera de Barcelona, 280"
+        }
+      ]
+    }
+    // useEffect(() => {
+    //   dibujar el componente (una sola vez o cuando se actualice)
+
+    //   return {
+    //     clean PaymentRequestUpdateEvent
+    //   }
+    // }, [routes])
+  ])
 
   return (
     <>
       < NavBar />
       <main>
-        <DropdownListRoutes />
-        <RouteInfoDisplay />
+        <DropdownListRoutes routes={routes} />
       </main>
     </>
   )
--- client/src/components/RouteInfoDisplay.jsx
+++ client/src/components/RouteInfoDisplay.jsx
@@ -1,31 +1,18 @@
-function RouteInfoDisplay () {
-  const stops = [
-    {
-      id: 1,
-      name: 'a'
-    },
-    {
-      id: 2,
-      name: 'b'
-    },
-    {
-      id: 3,
-      name: 'c'
-    }
-  ]
-
+function RouteInfoDisplay ({routeInfo}) {
   return (
     <div id="routeInfoDisplay">
-      <h1>Route name</h1>
       <div id="stopsContainer">
         <div id="stopsList">
-            {stops.map((stop) => {
-                return <p key={stop.id}>{stop.name}</p>
-            })}
+          <h3>{routeInfo[0].name}</h3>
+          <ul>
+            {routeInfo[0].stops.map((stop) => (
+            <li key={stop.id}>{stop.name}</li>
+            ))}
+          </ul>
         </div>
-        <div>Studens per stop</div>    
+        <div>Studens per stop</div>
       </div>
-    </div>  
+    </div> 
   )
 }
 
