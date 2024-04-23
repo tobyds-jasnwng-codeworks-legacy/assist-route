fd7c479d069605e97a6f6c66843bbaa73561b553
yulia <langedoc@gmail.com>
2024-04-23 14:19:14 +0200

    feat: delete student button working

--- client/src/components/StudentCard/StudentCard.jsx
+++ client/src/components/StudentCard/StudentCard.jsx
@@ -15,6 +15,15 @@ function StudentCard ({ students, selectedStudent, onClose}) {
   function camelToText (camelCase) {
     return camelCase.replace(/([A-Z])/g, ' $1').toLowerCase();
   }
+  
+  async function handleDelete () {
+    console.log('DELEEETE', studentData.id);
+    const res = await fetch('http://localhost:3000/students/'+studentData.id, {
+        method: "DELETE",
+        mode: "cors"
+    })
+  }
+  //DELETE STUDENT FROM LIST
 
   return (
     <div className="listContainer">
@@ -22,6 +31,7 @@ function StudentCard ({ students, selectedStudent, onClose}) {
       <div className="fieldsList">
         {studentInfoElements}
       </div>
+      <button type="button" name="deleteStudentButton" onClick={handleDelete}>DELETE</button>
     </div>
   );
 };
