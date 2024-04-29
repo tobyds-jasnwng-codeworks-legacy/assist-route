5abb263a8b602e7edf688a6ecf778e44c8a1cc20
TobyDS <tobydixonsmith@gmail.com>
2024-04-29 18:24:21 +0100

    refactor(server): :card_file_box: refactor db connection

--- server/models/students.js
+++ server/models/students.js
@@ -1,53 +1,51 @@
 'use strict';
-const { DataTypes } = require('sequelize');
-const db = require('./index');
 
-// Defining the Student model for Assist Route DB
-const Student = db.sequelize.define('Student', {
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  morningRoute: {
-    type: DataTypes.STRING,
-  },
-  morningStop: {
-    type: DataTypes.STRING,
-  },
-  eveningRoute: {
-    type: DataTypes.STRING,
-  },
-  eveningStop: {
-    type: DataTypes.STRING,
-  },
-  contactPerson1: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  contactPerson1Phone: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  contactPerson2: {
-    type: DataTypes.STRING,
-  },
-  contactPerson2Phone: {
-    type: DataTypes.STRING,
-  },
-  address: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  additionalInfo: {
-    type: DataTypes.STRING,
-  },
-  photo: {
-    type: DataTypes.STRING,
-  },
-});
-
-module.exports = Student;
+module.exports = (sequelize, DataTypes) => {
+  const Student = sequelize.define('Student', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    morningRoute: {
+      type: DataTypes.STRING,
+    },
+    morningStop: {
+      type: DataTypes.STRING,
+    },
+    eveningRoute: {
+      type: DataTypes.STRING,
+    },
+    eveningStop: {
+      type: DataTypes.STRING,
+    },
+    contactPerson1: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    contactPerson1Phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    contactPerson2: {
+      type: DataTypes.STRING,
+    },
+    contactPerson2Phone: {
+      type: DataTypes.STRING,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    additionalInfo: {
+      type: DataTypes.STRING,
+    },
+    photo: {
+      type: DataTypes.STRING,
+    },
+  });
+  return Student;
+};
