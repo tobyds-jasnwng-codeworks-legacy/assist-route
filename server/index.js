7e3d4d938f001e5fd95fd33e6f4c652d2337579e
TobyDS <tobydixonsmith@gmail.com>
2024-04-29 16:50:33 +0100

    refactor(server): :sparkles: update to use env variables

--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
 'use strict';
 const express = require('express');
-const app = express();
-const router = require('./router.js');
 const cors = require('cors');
+const router = require('./router.js');
 const db = require('./models/index.js');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'http://localhost';
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const port = 3000;
-
 // Synchronize the model with the database before connecting on port
 (async () => {
   await db.sequelize.sync();
   console.log('Database & tables created!');
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  app.listen(PORT, () => {
+    console.log(`Server running at ${HOST}:${PORT}`);
   });
 })();
