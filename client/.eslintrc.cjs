bb9d08e6b21096b7c832efd970fa25a2b2f92c36
TobyDS <tobydixonsmith@gmail.com>
2024-04-29 16:53:08 +0100

    chore(client): :bug: correct eslintrc options

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -18,8 +18,8 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'no-unused-vars': 'off',
-    'no-undef': 'off',
+    'no-unused-vars': 'warn',
+    'no-undef': 'warn',
     'no-console': 'off',
     eqeqeq: 'warn',
     'func-style': ['error', 'declaration'],
