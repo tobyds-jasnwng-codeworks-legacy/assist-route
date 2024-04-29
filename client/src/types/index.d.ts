45c511690fe103552611bdc68dabeefed3eb5785
Jason Wong <jasonwong_3@hotmail.com>
2024-04-29 18:02:35 +0100

    refactor(client): :recycle: typescript - allStudentsList

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -1,1 +1,18 @@
-declare module '*.module.css';
+export type Student = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  morningRoute: string;
+  morningStop: string;
+  eveningRoute: string;
+  eveningStop: string;
+  contactPerson1: string;
+  contactPerson1Phone: string;
+  contactPerson2: string;
+  contactPerson2Phone: string;
+  address: string;
+  additionalInfo: string;
+  photo: string;
+  createdAt: string;
+  updatedAt: string;
+};
