3e59d16df93ec203f25c52c6a46c477072c96a41
TobyDS <tobydixonsmith@gmail.com>
2024-04-30 14:19:20 +0100

    test(client): :white_check_mark: add NavBar test

--- client/src/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-function ErrorMessage ({ message = 'Something went wrong' }: ErrorMessageProps) {
-  return <div data-testid='message-container'>{message}</div>;
-}
-
-type ErrorMessageProps = {
-  message?: string;
-};
-
-export default ErrorMessage;
