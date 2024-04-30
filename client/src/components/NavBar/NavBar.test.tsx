dc5008a02b291642bb40c7dfb5b6da9031cace4e
TobyDS <tobydixonsmith@gmail.com>
2024-04-30 14:19:20 +0100

    test(client): :white_check_mark: add NavBar test

---
+++ client/src/components/NavBar/NavBar.test.tsx
@@ -0,0 +1,24 @@
+import { render, fireEvent } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+import NavBar from './NavBar';
+
+describe('NavBar', () => {
+  it('should render the project name', () => {
+    const { getByText } = render(<NavBar toggleStudentsList={() => {}} />);
+    expect(getByText('ASSIST ROUTE')).toBeInTheDocument();
+  });
+
+  it('should render the manage students button', () => {
+    const { getByText } = render(<NavBar toggleStudentsList={() => {}} />);
+    expect(getByText('Manage students')).toBeInTheDocument();
+  });
+
+  it('should call toggleStudentsList when manage students button is clicked', () => {
+    const mockToggleStudentsList = vi.fn();
+    const { getByText } = render(
+      <NavBar toggleStudentsList={mockToggleStudentsList} />
+    );
+    fireEvent.click(getByText('Manage students'));
+    expect(mockToggleStudentsList).toHaveBeenCalled();
+  });
+});
